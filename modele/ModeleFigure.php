f0f1be2be42892bff2104a19d2a402c5beffd9cb
Gaël Foppolo <me@gaelfoppolo.com>
2014-10-18 19:31:59 +0200

    ajout de méthodes
    la figure est-elle dans les forces/faiblesses de la figure this

--- modele/ModeleFigure.php
+++ modele/ModeleFigure.php
@@ -1,9 +1,17 @@
 <?php
 
-class abstract ModeleFigure {
+class static abstract ModeleFigure {
 
 	protected static $image; // pas sur de ça je pense on le mettra direct dans la vue non ?
 	protected static $son; // pareil ici
+	
+	public function estDansSesForces($figure) { // $figure est elle dans les forces de la figure this ?
+		return (in_array($figure, $forces));
+	}
+	
+	public function estDansSesFaiblesses() { // $figure est elle dans les faiblesses de la figure this ?
+		return (in_array($figure, $faiblesses)); // inutile, car si pas dans les forces alors dans les faiblesses
+	}
 	
 }
 
