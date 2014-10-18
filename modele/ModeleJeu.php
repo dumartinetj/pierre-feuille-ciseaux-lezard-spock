ed2a2d322645746986c467ed8e8e4ef1a342f790
Gaël Foppolo <me@gaelfoppolo.com>
2014-10-18 18:22:04 +0200

    ajout classe jeu

---
+++ modele/ModeleJeu.php
@@ -0,0 +1,22 @@
+<?php
+
+class ModeleJeu {
+
+    private $identifiant;
+	private $nbManche; // je sais pas si cet attribut sera utile - doit être impair et sup à 2
+	private $joueur;
+	
+    public function __construct($i, $j1, $nb) {
+        $this->identifiant = $i;
+        $this->joueur = $j1;
+        $this->nbManche = $nb;
+    }
+	
+	// je laisse ça là pour pas l'oublier
+	// comment ajouter dans un tableau quand on connait pas le nb d'objets
+	// array_push($array, $donnes);
+    // ou $array[] = $donnes; pour les objets simples
+	// stackoverflow : http://stackoverflow.com/questions/5385433/how-to-create-an-empty-array-in-php-with-predefined-size
+}
+
+?>
