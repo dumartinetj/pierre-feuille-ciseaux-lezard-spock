06335316d6c71e23cca1bbd5c45e57d987b7d0af
Gaël Foppolo <me@gaelfoppolo.com>
2014-10-19 15:58:36 +0200

    on rentre dans le jeu :p

--- jeu.php
+++ jeu.php
@@ -6,17 +6,25 @@
     </head>
     <body>
         <?php
-            define('ROOT', dirname(__FILE__));
-            define('DS', dirname(DIRECTORY_SEPARATOR));
+		
+			include('modele/ModeleJoueur.php');
+			include('modele/ModelePartie.php');
+			include('modele/ModeleManche.php');
+		
+            //define('ROOT', dirname(__FILE__));
+            //define('DS', dirname(DIRECTORY_SEPARATOR));
             //include ROOT . DS . 'controleur' . DS . 'ControleurCoup.php';
             //AFTER CHRONO
             //require_once ROOT . DS . 'controleur' . DS . 'ControleurCoup.php';
-			j1 = new Joueur(1, "Jean", "Homme", 25);
-			j2 = new Joueur(2, "Jeanne", "Femme", 22);
-			p1 = new Partie(1, j1, j2);
+			$j1 = new ModeleJoueur(1, "Jean", "Homme", 25);
+			$j2 = new ModeleJoueur(2, "Jeanne", "Femme", 22);
+			$p1 = new ModelePartie(1, $j1, $j2);
 			$nbmanche = 1;
-			while($nbmanche != 3) {
-				// et là on fait les coups ect
+			while ($nbmanche =! 3) {
+				$m = new Manche($nbmanche);
+				//reste du code à faire
+				$p1.ajoutManche($m);
+				$nbmanche++;  
 			}
 
         ?>
