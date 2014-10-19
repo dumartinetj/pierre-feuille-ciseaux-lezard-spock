39d2dd6672a50e45056a599825eadcd382b1738f
Gaël Foppolo <me@gaelfoppolo.com>
2014-10-19 15:36:57 +0200

    exemple ébauche v3

--- jeu.php
+++ jeu.php
@@ -8,13 +8,17 @@
         <?php
             define('ROOT', dirname(__FILE__));
             define('DS', dirname(DIRECTORY_SEPARATOR));
-            include ROOT . DS . 'controleur' . DS . 'ControleurCoup.php';
+            //include ROOT . DS . 'controleur' . DS . 'ControleurCoup.php';
             //AFTER CHRONO
-            require_once ROOT . DS . 'controleur' . DS . 'ControleurCoup.php';
+            //require_once ROOT . DS . 'controleur' . DS . 'ControleurCoup.php';
 			j1 = new Joueur(1, "Jean", "Homme", 25);
 			j2 = new Joueur(2, "Jeanne", "Femme", 22);
 			p1 = new Partie(1, j1, j2);
-			
+			$nbmanche = 1;
+			while($nbmanche != 3) {
+				// et là on fait les coups ect
+			}
+
         ?>
     </body>
 </html>
