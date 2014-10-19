67e072b9ea476f094241846a6036357aa8e41170
Forthofferl <fortho30@live.fr>
2014-10-19 21:26:21 +0200

    amélioration vueErreurFigure

--- vues/jeu/vueErreurFigure.php
+++ vues/jeu/vueErreurFigure.php
@@ -1,4 +1,31 @@
-<?php
-    echo $figure." n'est pas une figure valable. (Essayez: pierre, feuille, ciseaux, lezard, spock)";
-?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Erreur de figure</title>
+    </head>
+    <body>
+        <h1> La figure : <?php echo $figure ?> n'est pas une figure valable.</h1>    
+        <div>
+            <h2> Essayer avec les figures suivantes: </hé>
+			<ul>
+            <li>
+				Pierre
+            </li>
+			<li>
+				Feuille
+            </li>
+			<li>
+				Ciseau
+            </li>
+			<li>
+				Lézard
+            </li>
+			<li>
+				Spock
+            </li>
+			</ul>
+        </div>
+    </body>
+</html>
