f9d2e13ae0c19c36d76cdd043641740ccf74e7f9
maraisP <pierre.marais@hotmail.fr>
2014-10-20 23:21:01 +0200

    Rajout de estGagnantPartie() (à corriger)

--- modele/Partie.php
+++ modele/Partie.php
@@ -6,7 +6,8 @@ class Partie {
 	private $nbManche; // je sais pas si cet attribut sera utile - doit être impair et sup à 2
 	private $joueur1;
     private $joueur2;
-    private $listeManche; // array de manches : mini une manche -> nb de manche set
+    private $listeManche; // array de manches : mini une manche -> nb de manche set	
+    private $gagnantPartie;
 	
     public function __construct($i, $j1, $j2) {
         $this->identifiant = $i;
@@ -17,6 +18,14 @@ class Partie {
 	
 	public function ajoutManche($m) {
 		array_push($this->listeManche, $m); // aide sur stackoverflow : http://stackoverflow.com/questions/5385433/how-to-create-an-empty-array-in-php-with-predefined-size
+	}
+	
+	public function estGagnantPartie(){
+	if (listeManche(1).getGagnantManche()==listeManche(2).getGagnantManche()){
+	          gagnantPartie=listeManche(1).getGagnantManche();
+	}
+	else{ ajoutManche(new Manche($i));
+		gagnantPartie=listeManche(3).getGagnantManche();
 	}
 }
 
