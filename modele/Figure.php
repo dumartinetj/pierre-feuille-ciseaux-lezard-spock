38da199070586d6123cfa4ef50dfe0d1b5cf6cfe
Forthofferl <fortho30@live.fr>
2014-10-20 19:22:09 +0200

    Changement nom de la classe + correction du static

---
+++ modele/Figure.php
@@ -0,0 +1,28 @@
+<?php
+
+abstract class Figure {
+
+	protected static $image; // pas sur de ça je pense on le mettra direct dans la vue non ?
+	protected static $son; // pareil ici
+	
+	public function __construct() { }
+	
+	public function estDansSesForces($figure) { // $figure est elle dans les forces de la figure this ?
+		return (in_array($figure, $forces));
+	}
+	
+	public function estDansSesFaiblesses() { // $figure est elle dans les faiblesses de la figure this ?
+		return (in_array($figure, $faiblesses)); // utile, car si pas dans les forces et pas dans les faiblesses -> draw
+	}
+	
+	public function quiSuisJe() {
+		return __CLASS__;
+	}
+	
+	public static function afficher(){
+		return static::quiSuisJe();
+	}
+	
+}
+
+?>
