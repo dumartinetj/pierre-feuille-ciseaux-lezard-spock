90c28680f57b117716597370cdbcbe87054b930b
Forthofferl <fortho30@live.fr>
2014-10-20 19:19:37 +0200

    Changement du nom de la classe ModelePartie en Partie

---
+++ modele/Partie.php
@@ -0,0 +1,23 @@
+<?php
+
+class Partie {
+
+    private $identifiant;
+	private $nbManche; // je sais pas si cet attribut sera utile - doit être impair et sup à 2
+	private $joueur1;
+    private $joueur2;
+    private $listeManche; // array de manches : mini une manche -> nb de manche set
+	
+    public function __construct($i, $j1, $j2) {
+        $this->identifiant = $i;
+        $this->joueur1 = $j1;
+        $this->joueur2 = $j2;
+        $this->listeManche = array();
+    }
+	
+	public function ajoutManche($m) {
+		array_push($this->listeManche, $m); // aide sur stackoverflow : http://stackoverflow.com/questions/5385433/how-to-create-an-empty-array-in-php-with-predefined-size
+	}
+}
+
+?>
