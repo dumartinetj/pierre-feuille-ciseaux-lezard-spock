308fd90fba2405107df521c15916f8a084df0d8c
Gaël Foppolo <me@gaelfoppolo.com>
2014-10-28 14:10:28 +0100

    ajout des fichiers de configuration + connexion base de donnes

---
+++ config/Config.php
@@ -0,0 +1,36 @@
+<?php
+
+class Config {
+
+    private static $database = array(
+        'hostname' => '',
+        'database' => '',
+        'login'    => '',
+        'password' => ''
+    );
+
+    private static $debug = true;
+
+    static public function getLogin() {
+        return self::$database['login'];
+    }
+
+    static public function getHostname() {
+        return self::$database['hostname'];
+    }
+
+    static public function getDatabase() {
+        return self::$database['database'];
+    }
+
+    static public function getPassword() {
+        return self::$database['password'];
+    }
+
+    static public function getDebug() {
+        return self::$debug;
+    }
+
+}
+
+?>
---
+++ modele/Modele.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once ROOT . DS . 'config' . DS . 'Config.php';
+
+class Modele {
+
+    public static $pdo;
+
+    public static function set_static() {
+        $host = Config::getHostname();
+        $dbname = Config::getDatabase();
+        $login = Config::getLogin();
+        $pass = Config::getPassword();
+
+        try {
+            self::$pdo = new PDO("mysql:host=$host;dbname=$dbname", $login, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+            if (Config::getDebug()) {
+                echo $ex->getMessage();
+                die ("Problème lors de la connexion à la base de données");
+            } else {
+                echo "Une erreur est survenue. <a href=""> Retour a la page d'accueil </a>";
+            }
+            die();
+        }
+    }
+}
+Modele::set_static();
+
+?>
