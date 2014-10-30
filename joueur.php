8fff2023a15b26b84e85fcfb66cc39c65b23aeeb
Gaël Foppolo <me@gaelfoppolo.com>
2014-10-30 16:23:55 +0100

    ajout d'un define pour que vue.php fonctionne

--- joueur.php
+++ joueur.php
@@ -1,9 +1,11 @@
 <?php
     define('ROOT', dirname(__FILE__));
     define('DS', dirname(DIRECTORY_SEPARATOR));
+    define('BASE', str_replace('//', '/', dirname($_SERVER['PHP_SELF']). '/'));
     define('VIEW_PATH', ROOT.DS.'vue'.DS);
     define('CTR_PATH', ROOT.DS.'controleur'.DS);
     define('MODEL_PATH', ROOT.DS.'modele'.DS);
+    define('VIEW_PATH_BASE', BASE.'vue/');
     $page = 'joueur';
     if (isset($_GET['action']))
         $action = $_GET['action'];
