0fa2fefb23216df5d63228ca8636124446cb6c46
Gaël Foppolo <me@gaelfoppolo.com>
2014-10-31 12:30:21 +0100

    correction variable vueProfilJoueur

--- vue/joueur/vueProfilJoueur.php
+++ vue/joueur/vueProfilJoueur.php
@@ -1,14 +1,15 @@
 <h3>Votre profil</h3>
 
 <p>
-
+<?php
 echo <<< EOT
 Pseudo : $p <br/>
 Age : $a <br/>
 Sexe : $s <br/>
 E-mail : $e <br/>
-Nombre de victoire : $nbV <br/>
-Nombre de défaite : $nbD <br/>
+Nombre de victoire : $nbv <br/>
+Nombre de défaite : $nbd <br/>
 Ratio : $r <br/>
 EOT;
+?>
 </p>
