37c287ff93596bc1f6f6503443129d634ee0e600
maraisP <pierre.marais@hotmail.fr>
2014-11-04 00:36:47 +0100

    Ajout descriptif règles+ image

--- vue/index/vueReglesIndex.php
+++ vue/index/vueReglesIndex.php
@@ -3,7 +3,27 @@
 <h3>Règles du jeu</h3>
 <hr>
 <p>Une petite explication du jeu par Sheldon Cooper</p>
+<p>
 <div class="embed-responsive embed-responsive-16by9">
   <iframe class="embed-responsive-item" src="//www.youtube.com/embed/llq528pnv9c"></iframe>
 </div>
+</p>
+<p>
+	Si vous n'avez pas tous suivis voici un récapitulatif des régles accompagnés d'un schéma explicatif.
+</p>
+
+<p>	
+	<ul> 
+		<li> La pierre écrase le lézard </li> 
+		<li> Le lézard empoisonne Spock </li> 
+		<li> Spock casse les ciseaux </li> 
+		<li> Les ciseaux décapitent le lézard </li>
+		<li> Le lézard mange la feuille </li> 
+		<li> La feuille désavoue Spock </li> 
+		<li> Spock vaporise la pierre </li> 
+	</ul>
+</p>
+<p>
+<img src="123.png" alt="Schéma des régles" />
+</p>
 </div>
