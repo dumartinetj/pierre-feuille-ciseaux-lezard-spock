268a011059f92456b5f9c516dbad4d39165ffbe2
Gaël Foppolo <me@gaelfoppolo.com>
2014-11-04 21:46:09 +0100

    correction orthographe

--- vue/jeu/vueRechercheJeu.php
+++ vue/jeu/vueRechercheJeu.php
@@ -1,7 +1,7 @@
 <h3>Recherche d'un adversaire</h3>
 <hr>
 <p>Veuillez saisir le nombre de manches que vous voulez jouer au maximum<br/>
-Pour connaître les règles du jeu et le déroulemen d'une partie, rendez-vous sur la <a href="index.php?action=regles">pages des règles du jeu</a></p>
+Pour connaître les règles du jeu et le déroulement d'une partie, rendez-vous sur la <a href="index.php?action=regles">page des règles du jeu</a></p>
 <div class="col-md-3"></div>
 <div class="col-md-6">
 <form method="post" action="jouer.php?action=rechercher">
