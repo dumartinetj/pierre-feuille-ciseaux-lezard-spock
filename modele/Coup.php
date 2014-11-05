013b911576913e8e8effc29ca30ea26eb200ac59
Gaël Foppolo <me@gaelfoppolo.com>
2014-11-05 23:11:23 +0100

    MAJ des fonctions, fonctionnent avec la BDD now, sans objet

--- modele/Coup.php
+++ modele/Coup.php
@@ -4,84 +4,140 @@
  * Classe Coup
  */
 
-class Coup {
+require_once 'Figure.php';
+require_once 'Modele.php';
 
-    private $identifiant;
-    private $figureJoueur1;
-    private $figureJoueur2;
-    private $joueur1;
-    private $joueur2;
-    private $gagnant;
+class Coup extends Modele {
 
-	/*
-	 * Constructeur de la classe qui instancie un nouveau Coup
-	 */
-
-    public function __construct($i, $fj1, $fj2, $j1, $j2) {
-        $this->identifiant = $i;
-        $this->figureJoueur1 = $fj1;
-        $this->figureJoueur2 = $fj2;
-        $this->joueur1 = $j1;
-        $this->joueur2 = $j2;
-        $this->gagnant = NULL;
+    public static function getIDFigureJoueur1($id) {
+        try {
+                $req = self::$pdo->prepare("SELECT idFigure1 FROM pfcls_Coups WHERE idCoup=".$id);
+                $req->execute();
+                if ($req->rowCount() != 0) {
+                    $data_recup = $req->fetch(PDO::FETCH_OBJ);
+                    return $data_recup->idFigure1;
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                die("Erreur lors de la récupération de l'ID de la figure du joueur 1 du coup dans la BDD");
+            }
     }
 
-	/*
-	 * Évalue le coup et set le gagnant et le perdant
-	 */
-    public function evaluer() {
-            $f1 = $this->getFigureJoueur1();
-            $f2 = $this->getFigureJoueur2();
-            $f1win = $f1->estDansSesForces($f2->getIdentifiant());
-            if ($f1win) {
-                $this->gagnant = $this->joueur1;
+    public static function getIDFigureJoueur2($id) {
+        try {
+                $req = self::$pdo->prepare("SELECT idFigure2 FROM pfcls_Coups WHERE idCoup=".$id);
+                $req->execute();
+                if ($req->rowCount() != 0) {
+                    $data_recup = $req->fetch(PDO::FETCH_OBJ);
+                    return $data_recup->idFigure2;
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                die("Erreur lors de la récupération de l'ID de la figure du joueur 2 du coup dans la BDD");
+            }
+    }
+
+    public static function getIDJoueurGagnantCoup($id){
+        try {
+                $req = self::$pdo->prepare("SELECT idJoueurGagnant FROM pfcls_Coups WHERE idCoup=".$id);
+                $req->execute();
+                if ($req->rowCount() != 0) {
+                    $data_recup = $req->fetch(PDO::FETCH_OBJ);
+                    return $data_recup->idJoueurGagnant;
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                die("Erreur lors de la récupération de l'ID du joueur gagnant du coup dans la BDD");
+            }
+    }
+
+    public static function getIDJoueur1($id){
+        try {
+                $req = self::$pdo->prepare("SELECT idJoueur1 FROM pfcls_Coups WHERE idCoup=".$id);
+                $req->execute();
+                if ($req->rowCount() != 0) {
+                    $data_recup = $req->fetch(PDO::FETCH_OBJ);
+                    return $data_recup->idJoueur1;
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                die("Erreur lors de la récupération de l'ID du joueur 1 du coup dans la BDD");
+            }
+    }
+
+    public static function getIDJoueur2($id){
+        try {
+                $req = self::$pdo->prepare("SELECT idJoueur2 FROM pfcls_Coups WHERE idCoup=".$id);
+                $req->execute();
+                if ($req->rowCount() != 0) {
+                    $data_recup = $req->fetch(PDO::FETCH_OBJ);
+                    return $data_recup->idJoueur2;
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                die("Erreur lors de la récupération de l'ID du joueur 2 du coup dans la BDD");
+            }
+    }
+
+    public static function ajoutCoup($data) {
+        try {
+                $req = self::$pdo->prepare('INSERT INTO pfcls_Coups (idJoueur1, idJoueur2) VALUES (:idJoueur1, :idJoueur2)');
+                $req->execute($data);
+                return self::$pdo->lastInsertId(); //retourne le dernier id insérer dans la BDD sur cette session
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                $messageErreur="Erreur lors de l'insertion d'un coup dans la BDD";
+            }
+    }
+
+    public static function deleteCoup($id) {
+        try {
+                $req = self::$pdo->prepare("DELETE FROM pfcls_Coups WHERE idCoup=".$id);
+                $req->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                $messageErreur="Erreur lors de la supression d'un coup dans la BDD";
+            }
+    }
+
+    public static function setGagnant($id, $idJGagnant){
+        try {
+                $req = self::$pdo->prepare("UPDATE pfcls_Coups SET idJoueurGagnant=".$idJGagnant." WHERE idCoup=".$id);
+                $req->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                die("Erreur lors de la MAJ de l'ID du joueur gagnant du coup dans la BDD");
+            }
+    }
+
+    public static function evaluer($id) {
+            $idF1 = self::getIDFigureJoueur1($id);
+            $idF2 = self::getIDFigureJoueur2($id);
+            $idJ1 = self::getIDJoueur1($id);
+            $idJ2 = self::getIDJoueur2($id);
+            $J1Win = Figure::estDansSesForces($idF2,$idF1);
+            $J1Win = ($J1Win && Figure::estDansSesFaiblesses($idF1,$idF2));
+            if ($J1Win) {
+                self::setGagnant($id,$idJ1);
             }
             else {
-                $this->gagnant = $this->joueur2;
+                self::setGagnant($id,$idJ2);
             }
     }
 
-	/*
-	 * Compare les deux figures d'un coup
-	 * @return vrai si les figures sont identiques, faux si non
-	 */
-    public function estUnDraw() {
-        $fj1 = $this->figureJoueur1;
-        $fj2 = $this->figureJoueur2;
-        $id1 = $fj1->getIdentifiant();
-        $id2 = $fj2->getIdentifiant();
-        return ($id1==$id2);
+    public static function estUnDraw($id) {
+        $idF1 = self::getIDFigureJoueur1($id);
+        $idF2 = self::getIDFigureJoueur2($id);
+        return ($idF1 == $idF2);
     }
 
-	/*
-	 * Concatene les ids des figures jouées par les 2 joueurs
-	 * @return une string contenant les ids des figures
-	 */
-    public function retourneIDs(){
-	     $fj1 = $this->figureJoueur1;
-       $fj2 = $this->figureJoueur2;
-	     $ids = ($fj1->getIdentifiant()).($fj2->getIdentifiant());
-	     return $ids;
+    public static function retourneIDs($id){ // pour les stats
+	     $idF1 = self::getIDFigureJoueur1($id);
+       $idF2 = self::getIDFigureJoueur2($id);
+	     return $idF1.$idF2;
     }
 
-    public function getFigureJoueur1() {
-        return $this->figureJoueur1;
-    }
 
-    public function getFigureJoueur2() {
-        return $this->figureJoueur2;
-    }
-
-    public function getJoueurGagnantCoup(){
-        return $this->gagnant;
-    }
-
-    public function getJoueur1(){
-        return $this->joueur1;
-    }
-
-    public function getJoueur2(){
-        return $this->joueur2;
-    }
 }
+
 ?>
--- testStatic.php
+++ testStatic.php
@@ -10,11 +10,13 @@
 
 define('ROOT', dirname(__FILE__));
 define('DS', dirname(DIRECTORY_SEPARATOR));
-include('modele/Figure.php');
+include_once('modele/Figure.php');
 include_once('modele/Joueur.php');
 include_once('modele/Partie.php');
 include_once('modele/Manche.php');
 include_once('modele/Coup.php');
+
+
 
 /*$data = array(
             'pseudo' => "testinscription2",
@@ -26,7 +28,7 @@ include_once('modele/Coup.php');
             'email' => "testinsc2@gmail.com"
 );
 $var = Joueur::inscription($data);
-var_dump($var);*/
+var_dump($var);
 
 $data2 = array(
             'pseudo' => "testinscription2",
@@ -43,13 +45,13 @@ $nbmanche = 1;
 $nbcoup=1;
 
 echo '-- Début de la partie! --<br/>';
-            
+
 	        // nom: jeu
 		// description: contient toutes les instructions pour jouer une partie
 		// param: rien
 		// retourne: retourne le gagnant de la partie
-            
-            while (!$partie->checkPartieFinie()) {  
+
+            while (!$partie->checkPartieFinie()) {
 		echo '--- Début de la manche '.$nbmanche.'! ---<br/>';
                 $manche = new Manche($nbmanche);
                 $nbcoup=1;
@@ -92,24 +94,29 @@ $f3 = new Figure(3);
 $f4 = new Figure(4);
 $f5 = new Figure(5);
 
-echo($f3->getIdentifiant());
-echo($f4->getNom());
-print_r ($f3->getForces());
-print_r ($f3->getFaiblesses());
-$id2 = $f2->getIdentifiant();
-$res = $f3->estDansSesFaiblesses($id2);
-var_dump($res);
-/*$res = $f3->estDansSesFaiblesses($id2);
-var_dump($res);
-if ($res)
-  {
-  echo "Dans ses forces";
-  }
-else
-  {
-  echo "Dans ses faiblesses";
-}*/
 
+echo(Figure::getNom(1));
+echo(Figure::getNom(5));
+print_r (Figure::getForces(3));
+print_r (Figure::getFaiblesses(2));
+$res = Figure::estDansSesFaiblesses(1,2);
+var_dump($res);
+$res2 = Figure::estDansSesForces(1,2);
+var_dump($res2); */
+
+$data = array(
+            'idJoueur1' => 7,
+            'idJoueur2' => 8,
+);
+//$id = Coup::ajoutCoup($data);
+var_dump(Coup::getIDFigureJoueur1(5));
+var_dump(Coup::getIDFigureJoueur2(5));
+var_dump(Coup::getIDJoueur1(5));
+var_dump(Coup::getIDJoueur2(5));
+var_dump(Coup::estUnDraw(5));
+var_dump(Coup::retourneIDs(5));
+Coup::evaluer(5);
+var_dump(Coup::getIDJoueurGagnantCoup(5));
 
 ?>
 
