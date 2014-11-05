aa06ff7dc840f7da86c73434b4559c76160bfc3e
Gaël Foppolo <me@gaelfoppolo.com>
2014-11-05 21:46:46 +0100

    mise à jour des fonctions, fonctionnent avec la BDD now, sans objet

--- modele/Figure.php
+++ modele/Figure.php
@@ -8,91 +8,67 @@ require_once 'Modele.php';
 
 class Figure extends Modele {
 
-	protected $identifiant;
-	/* Liste des forces de la figure */
-	protected $forces;
-	/* Liste des faiblesses de la figure */
-	protected $faiblesses;
+    public static function getNom($id) {
+			try {
+							$req = self::$pdo->prepare("SELECT nom FROM pfcls_Figures WHERE idFigure=".$id);
+							$req->execute();
+							if ($req->rowCount() != 0) {
+									$data_recup = $req->fetch(PDO::FETCH_OBJ);
+									return $data_recup->nom;
+							}
+					} catch (PDOException $e) {
+							echo $e->getMessage();
+							die("Erreur lors de la récupération du nom de la figure dans la BDD");
+					}
+		}
 
-	/*
-	 * Constructeur de la classe qui instancie une nouvelle Figure
-	 */
+		public static function getForces($id){ // retourne un array
+				try {
+							$req = self::$pdo->prepare("SELECT forces FROM pfcls_Figures WHERE idFigure=".$id);
+							$req->execute();
+							if ($req->rowCount() != 0) {
+									$data_recup = $req->fetch(PDO::FETCH_OBJ);
+									$forces = explode(",",$data_recup->forces);
+									return $forces;
+							}
+				} catch (PDOException $e) {
+							echo $e->getMessage();
+							die("Erreur lors de la récupération des forces de la figure dans la BDD");
+					}
+			}
 
-    public function __construct($id) {
-		$this->identifiant = $id;
-		try {
-            $req = self::$pdo->prepare("SELECT * FROM pfcls_Figures WHERE idFigure=".$id);
-            $req->execute();
-            if ($req->rowCount() != 0) {
-                $data_recup = $req->fetch(PDO::FETCH_OBJ);
-                $this->forces = explode(",",$data_recup->forces);
-								$this->faiblesses = explode(",",$data_recup->faiblesses);
+			public static function getFaiblesses($id){ //retourne un array
+					try {
+								$req = self::$pdo->prepare("SELECT faiblesses FROM pfcls_Figures WHERE idFigure=".$id);
+								$req->execute();
+								if ($req->rowCount() != 0) {
+										$data_recup = $req->fetch(PDO::FETCH_OBJ);
+										$faiblesses = explode(",",$data_recup->faiblesses);
+										return $faiblesses;
+								}
+					} catch (PDOException $e) {
+								echo $e->getMessage();
+								die("Erreur lors de la récupération des faiblesses de la figure dans la BDD");
 						}
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            die("Erreur lors de l'instanciation d'une figure");
-        }
-
-	}
+			}
 
 	/*
-	 * Getter de l'identifiant de la figure
-	 * @return l'identifiant de la figure
-	 */
-	public function getIdentifiant() {
-		return $this->identifiant;
-	}
-
-	/*
-	 * Retourne le nom de la figure à partir de son id
-	 * @return le nom de la figure
-	 */
-	public function getNom() {
-		try {
-            $req = self::$pdo->prepare("SELECT nom FROM pfcls_Figures WHERE idFigure=".$this->identifiant);
-            $req->execute();
-            if ($req->rowCount() != 0) {
-                $data_recup = $req->fetch(PDO::FETCH_OBJ);
-                 return $data_recup->nom;
-						}
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            die("Erreur lors de la r?cup?ration du nom d'une figure");
-        }
-	}
-	/*
-	 * Getter des forces de la figure
-	 * @return la liste des forces de la figure
-	 */
-	public function getForces(){
-        return $this->forces;
-    }
-
-	/*
-	 * Getter des faiblesses de la figure
-	 * @return la liste des faiblesses de la figure
-	 */
-    public function getFaiblesses(){
-        return $this->faiblesses;
-    }
-
-	/*
-	 * Vérifie si la figure passé en paramètre est dans les forces de la figure
-	 * @param l'id de la figure à comparer
+	 * Vérifie si la figure passé en premier paramètre est dans les forces de la figure en deuxième param
 	 * @return un booléen, vrai si elle est dans ses forces, faux sinon
 	 */
-	public function estDansSesForces($idfigure) {
-		return in_array($idfigure, $this->forces);
-	}
+			public static function estDansSesForces($idFigure1, $idFigure2) {
+					$forcesFigure2 = self::getForces($idFigure2);
+					return in_array($idFigure1, $forcesFigure2);
+			}
 
 	/*
-	 * Vérifie si la figure passé en paramètre est dans les forces de la figure
-	 * @param l'id de la figure à comparer
+	 * Vérifie si la figure passé en premier paramètre est dans les faiblesses de la figure en deuxième param
 	 * @return un booléen, vrai si elle est dans ses faiblesses, faux sinon
 	 */
-	public function estDansSesFaiblesses($idfigure) {
-		return in_array($idfigure, $this->faiblesses);
-	}
+			public static function estDansSesFaiblesses($idFigure1, $idFigure2) {
+					$faiblessesFigure2 = self::getFaiblesses($idFigure2);
+					return in_array($idFigure1, $faiblessesFigure2);
+			}
 
 }
 
