47ad2db013f5105832f6e118807a20522d42b432
Gaël Foppolo <me@gaelfoppolo.com>
2014-11-15 22:12:49 +0100

    modification graphique, aucune modification contenu

--- vue/index/vueDefaultIndex.php
+++ vue/index/vueDefaultIndex.php
@@ -1,21 +1,40 @@
 		<h1 id="mainhead">PFCLS - Jeu en ligne</h1>
 		<hr>
 		<p>
-			Bienvenue sur le site PFCLS, ce site contient le jeu Pierre Feuille Ciseaux amélioré par l'ajout de Lézard et Spock.
-			Les règles du jeu vous serons expliqué par Sheldon Cooper le personnage de la série The Big Bang Theory. Un schéma récapitulatif des règles est aussi présent <a href="http://pfcls.me/index.php?action=regles">ici</a>.
+			Bienvenue sur le site PFCLS, ce site contient le jeu <em>Pierre Feuille Ciseaux amélioré</em> par l'ajout de Lézard et Spock.
+			Le jeu popularisé a été popularisé par la série <em>The Big Bang Theory</em>. Les règles sont présentes <a href="http://pfcls.me/index.php?action=regles">ici</a>.
 		</p>
-		<p>Passons maintenant à l'explication du pourquoi de ce site.
-		 Le but premier de ce projet est de recueillir des statistiques sur les joueurs, à terme on pourra savoir par exemple si une femme
-				est meilleure qu'un homme à ce jeu ou de déterminer si les hommes de 20 à 30 ans sont meilleurs que les hommes de 30 à 40 ans.
-			</p>
-			<p>
-				Ce sont en somme des statistiques basiques, mais qui vont servir ensuite pour programmer une Intelligence Artificielle (IA). Ce nouvel adversaire pourra selon votre âge et
-				votre sexe déterminer les coups que vous allez jouer pour pouvoir les contrer du mieux possible. L'IA pourra prévoir le coup qu'il faut jouer pour gagner contre vous, en cela, elle se comporte comme
-				l'IA d'un jeu d'échec qui prédit le meilleur coup à jouer grâce à l'expérience accumulée au cours de nombreuses parties. A chaque manches ce "robot" récupère les données pour être le plus précis possible.
-			</p>
+		<hr class="featurette-divider">
+		<div class="row featurette">
+	        <div class="col-md-7">
+	          <h2 class="featurette-heading">Présentation du projet<br> <span class="text-muted"></span></h2>
+	          <p class="lead">Le but premier de ce projet est de recueillir des statistiques sur les joueurs, à terme on pourra savoir par exemple si une femme
+							est meilleure qu'un homme à ce jeu ou de déterminer si les hommes de 20 à 30 ans sont meilleurs que les hommes de 30 à 40 ans.</p>
+	        </div>
+	        <div class="col-md-5">
+						<img class="featurette-image img-responsive" src="http://placehold.it/500x200" alt="">
+						<div class="caption">
+							<h3>Blablacaca</h3>
+						</div>
+	        </div>
+	  </div>
+		<hr class="featurette-divider">
+		<div class="row featurette">
+					<div class="col-md-5">
+						<img class="featurette-image img-responsive" src="http://placehold.it/500x400" alt="">
+						<div class="caption">
+							<h3>Blablacaca</h3>
+						</div>
+					</div>
+					<div class="col-md-7">
+						<h2 class="featurette-heading">Intelligence Artificielle<br> <span class="text-muted"></span></h2>
+						<p class="lead">Ce sont en somme des statistiques basiques, mais qui vont servir ensuite pour programmer une Intelligence Artificielle (IA). Ce nouvel adversaire pourra selon votre âge et
+							votre sexe déterminer les coups que vous allez jouer pour pouvoir les contrer du mieux possible. L'IA pourra prévoir le coup qu'il faut jouer pour gagner contre vous, en cela, elle se comporte comme
+							l'IA d'un jeu d'échec qui prédit le meilleur coup à jouer grâce à l'expérience accumulée au cours de nombreuses parties. A chaque manches ce "robot" récupère les données pour être le plus précis possible.</p>
+					</div>
+		</div>
+		<hr class="featurette-divider">
 			<p>
 				Évidemment, le second but de ce site est de divertir les joueurs en fournissant un système de jeu qui permet d'éviter les coups nuls (les deux joueurs utilisent la même figure) grâce à l'ajout
-				Des figures Lézard et Spock. Les joueurs ont donc besoin de moins de coups pour parvenir à gagner une manche, le jeu s'en trouve donc plus intéressant, car les parties peuvent s'enchaîner plus rapidement.
+				des figures Lézard et Spock. Les joueurs ont donc besoin de moins de coups pour parvenir à gagner une manche, le jeu s'en trouve donc plus intéressant, car les parties peuvent s'enchaîner plus rapidement.
 			</p>
-
-	<p>(faire l'explication du site, du projet, mettre des liens vers les règles, etc.)</p>
--- vue/index/vueReglesIndex.php
+++ vue/index/vueReglesIndex.php
@@ -1,34 +1,43 @@
     <h1 id="mainhead">Règles du jeu</h1>
     <hr>
-  <p>Une petite explication du jeu par Sheldon Cooper</p>
+  <p>Une petite explication en vidéo du jeu par <em>Sheldon Cooper</em> de la série <em>The Big Bang Theory</em> (en anglais)</p>
   <p>
   <div class="embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/llq528pnv9c"></iframe>
+    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/_PUEoDYpUyQ"></iframe>
   </div>
-  </p>
-  <p>
-  	Si vous n'avez pas tous suivis voici un récapitulatif de la règle du jeu accompagnée d'un schéma explicatif.
-  </p>
-
-  <p>
-  	<ul>
-  		<li> La pierre écrase le lézard </li>
-  		<li> Le lézard empoisonne Spock </li>
-  		<li> Spock casse les ciseaux </li>
-  		<li> Les ciseaux décapitent le lézard </li>
-  		<li> Le lézard mange la feuille </li>
-  		<li> La feuille désavoue Spock </li>
-  		<li> Spock vaporise la pierre </li>
-  	</ul>
-  </p>
-  <p>
-  	Pour démarrer une partie vous devais indiquer le nombre de manches que vous voulez jouer, ce nombre doit être
-  	impair pour qu'une égalité ne soit pas possible. De plus, le nombre de manches maximum est de 9 pour éviter que les parties soient trop longues.
-  	Pour gagner une partie il faut remporter assez de manche pour que l'adversaire, même s'il gagne toutes les manches restantes ne puisse pas remporter la victoire.
-  	Par exemple si vous jouez une partie en cinq manches et que vous remportez les trois premières, vous avez gagné la partie et vous ne jouerez donc pas les manches restantes votre victoire étant déjà assurée.
-
   </p>
   <hr>
   <p>
-  	<img src="<?php echo VIEW_PATH_BASE.'index/img/regles.png'?>" alt="Schéma des règles"/>
+
+  </p>
+  <div class="row featurette">
+  <div class="col-md-7">
+    <h2 class="featurette-heading">Si vous n'avez pas tous suivi, voici un récapitulatif des règles du jeu :</h2>
+  <p class="lead">
+  	<ul class="list-group">
+  		<li class="list-group-item"> La pierre écrase le lézard </li>
+  		<li class="list-group-item"> Le lézard empoisonne Spock </li>
+  		<li class="list-group-item"> Spock casse les ciseaux </li>
+  		<li class="list-group-item"> Les ciseaux décapitent le lézard </li>
+  		<li class="list-group-item"> Le lézard mange la feuille </li>
+  		<li class="list-group-item"> La feuille désavoue Spock </li>
+  		<li class="list-group-item"> Spock vaporise la pierre </li>
+  	</ul>
+  </p>
+  </div>
+  <div class="col-md-5">
+      <img class="featurette-image img-responsive" height="90%" width="90%" src="<?php echo VIEW_PATH_BASE.'index/img/regles.png'?>" alt="Schéma des règles">
+      <div class="caption">
+        <h3>Schéma des règles</h3>
+        <small><em>Source : Wikipedia</em></small>
+      </div>
+  </div>
+  </div>
+  <hr>
+  <p>
+   Pour démarrer une partie, vous deviez indiquer le nombre de manches que vous voulez jouer, ce nombre doit être
+   impair pour qu’une égalité ne soit pas possible. De plus, le nombre de manches maximum est de 9 pour éviter que les parties soient trop longues.
+   Pour gagner une partie, il faut remporter assez de manches pour que l’adversaire, même s’il gagne toutes les manches restantes, ne puisse pas remporter la victoire.
+   Par exemple si vous jouez une partie en cinq manches et que vous remportez les trois premières, vous avez gagné la partie et vous ne jouerez donc pas les manches restantes votre victoire étant déjà assurée.
+
   </p>
