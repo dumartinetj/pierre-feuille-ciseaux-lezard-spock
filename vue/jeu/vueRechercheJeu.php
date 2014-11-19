1220111fd719045ff6a0798d498f90aa9545bba7
Gaël Foppolo <me@gaelfoppolo.com>
2014-11-19 18:50:03 +0100

    changement par des boutons, évite l'utilisation du clavier

--- vue/jeu/vueRechercheJeu.php
+++ vue/jeu/vueRechercheJeu.php
@@ -1,10 +1,47 @@
 <h1 id="mainhead">Recherche d'un adversaire</h1>
 <hr>
-<p>Veuillez saisir le nombre de manches que vous voulez jouer au maximum<br/>
+<p>Choisissez le nombre de manches que vous voulez jouer !<br/>
 Pour connaître les règles du jeu et le déroulement d'une partie, rendez-vous sur la <a href="index.php?action=regles">page des règles du jeu</a></p>
-<form method="post" action="jouer.php?action=rechercher">
-    <fieldset>
-            <input type="number" class="form-control" placeholder="Nombre de manches" name="nbManche" id="id_nbManche" min="1" max="9" step="2" required/><br/>
-            <input type="submit" class="btn" value="Rechercher votre partie !" />
-    </fieldset>
-</form>
+
+<div class="row">
+  <div class="col-md-1">
+  </div>
+  <div class="col-md-2">
+    <form method="post" action="jouer.php?action=rechercher">
+      <input type="hidden" name="nbManche" id="id_nbManche" value="1">
+      <button type="submit" class="btn btn-info btn-lg">1</button>
+    </form>
+  </div>
+  <div class="col-md-1">
+  </div>
+  <div class="col-md-2">
+    <form method="post" action="jouer.php?action=rechercher">
+      <input type="hidden" name="nbManche" id="id_nbManche" value="3">
+      <button type="submit" class="btn btn-success btn-lg">3</button>
+    </form>
+  </div>
+  <div class="col-md-1">
+  </div>
+  <div class="col-md-2">
+    <form method="post" action="jouer.php?action=rechercher">
+      <input type="hidden" name="nbManche" id="id_nbManche" value="5">
+      <button type="submit" class="btn btn-primary btn-lg">5</button>
+    </form>
+  </div>
+  <div class="col-md-1">
+  </div>
+  <div class="col-md-2">
+    <form method="post" action="jouer.php?action=rechercher">
+      <input type="hidden" name="nbManche" id="id_nbManche" value="7">
+      <button type="submit" class="btn btn-warning btn-lg">7</button>
+    </form>
+  </div>
+  <div class="col-md-1">
+  </div>
+  <div class="col-md-2">
+    <form method="post" action="jouer.php?action=rechercher">
+      <input type="hidden" name="nbManche" id="id_nbManche" value="9">
+      <button type="submit" class="btn btn-danger btn-lg">9</button>
+    </form>
+  </div>
+</div>
