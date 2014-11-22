9f040b5aacf56114ad1d8ff2feffcd638d277c5c
Gaël Foppolo <me@gaelfoppolo.com>
2014-11-22 14:12:58 +0100

    description partie à propos complétée

--- vue/index/vueAproposIndex.php
+++ vue/index/vueAproposIndex.php
@@ -8,8 +8,8 @@
             <img class="img-circle" src="http://placehold.it/100x100" alt="">
             <div class="caption">
             <h2>Julien Dumartinet</h2>
-            <h3>Étudiant</h3>
-            <p>blablacaca</p>
+            <h2><small>Étudiant</small></h2>
+            blablacaca<br/>
             <p>
               <a class="btn btn-default" href="#" target="_blank" role="button">Lien</a>
               <a class="btn btn-default" href="#" target="_blank" role="button">Lien</a>
@@ -22,8 +22,8 @@
             <img class="img-circle" src="http://placehold.it/100x100" alt="">
             <div class="caption">
             <h2>Pierre Marais</h2>
-            <h3>Étudiant</h3>
-            <p>J'aime les jeux vidéo, les Harley et le Metal.</p>
+            <h2><small>Étudiant</small></h2>
+            J'aime les jeux vidéo, les Harley et le Metal.<br/>
             <p>
               <a class="btn btn-default" href="#" target="pierre.marais@hotmail.fr" role="button">pierre.marais@hotmail.fr</a>
             </p>
@@ -35,8 +35,8 @@
             <img class="img-circle" src="http://placehold.it/100x100" alt="">
             <div class="caption">
             <h2>Loïc Forthoffer</h2>
-            <h3>Étudiant</h3>
-            <p>blablacaca</p>
+            <h2><small>Étudiant</small></h2>
+            blablacaca<br/>
             <p>
               <a class="btn btn-default" href="#" target="_blank" role="button">Lien</a>
               <a class="btn btn-default" href="#" target="_blank" role="button">Lien</a>
@@ -46,25 +46,26 @@
           <div class="col-md-1">
           </div>
           <div class="col-md-2">
-            <img class="img-circle" src="http://placehold.it/100x100" alt="">
+            <img class="img-circle img-responsive center-block" src="<?php echo VIEW_PATH_BASE.'index/img/gael.png'?>" style="width: 100px; height: 100px;" alt="Gaël">
             <div class="caption">
             <h2>Gaël Foppolo</h2>
-            <h3>Étudiant</h3>
-            <p>blablacaca</p>
+            <h2><small>Étudiant</small></h2>
+            Rock, métal, séries, mangas, projets novateurs et cannelloni<br/>
             <p>
-              <a class="btn btn-default" href="#" target="_blank" role="button">Lien</a>
-              <a class="btn btn-default" href="#" target="_blank" role="button">Lien</a>
+              <a href="http://www.gaelfoppolo.com" class="btn btn-default btn-xs" target="_blank"><span class="glyphicon glyphicon-user"></span> Site web</a>
+              <a href="mailto:me@gaelfoppolo.com" target="_top" class="btn btn-info btn-xs"><span class="glyphicon glyphicon-envelope"></span> E-mail</a>
             </p>
             </div>
           </div>
           <div class="col-md-1">
+            
           </div>
           <div class="col-md-2">
             <img class="img-circle" src="http://placehold.it/100x100" alt="">
             <div class="caption">
             <h2>Madalina Croitoru</h2>
-            <h3>Tutrice</h3>
-            <p>blablacaca</p>
+            <h2><small>Tutrice</small></h2>
+            blablacaca<br/>
             <p>
               <a class="btn btn-default" href="#" target="_blank" role="button">Lien</a>
               <a class="btn btn-default" href="#" target="_blank" role="button">Lien</a>
