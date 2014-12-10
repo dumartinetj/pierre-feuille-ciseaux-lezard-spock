1a144bd4a94b35add78be8f82064afdfa644b786
Gaël Foppolo <me@gaelfoppolo.com>
2014-12-10 22:27:31 +0100

    suppression de vues inutiles

--- vue/joueur/vueConnecteJoueur.php
+++
@@ -1,3 +0,0 @@
-<h1 id="mainhead">Vous êtes maintenant connecté !</h1>
-<hr>
-<p> <a href="index.php">Revenir à l'index</a> </p>
--- vue/joueur/vueDeconnexionJoueur.php
+++
@@ -1,2 +0,0 @@
-<h1 id="mainhead">Vous êtes maintenant déconnecté !</h1>
-<p> <a href="index.php">Revenir à l'index</a> </p>
