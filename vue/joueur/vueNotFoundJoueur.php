3bd0ff55f74deb2d3495c468f439efe551817d5b
Gaël Foppolo <me@gaelfoppolo.com>
2014-12-22 12:24:30 +0100

    suppresion vues inutiles

--- vue/joueur/vueNotFoundJoueur.php
+++
@@ -1,4 +0,0 @@
-<h1 id="mainhead">Aucun joueur trouvé !</h1>
-<hr>
-<p>Aucun joueur ne correspond à ce pseudo :(</p>
-<a href='joueur.php?action=search'>Effectuer une nouvelle recherche avec un pseudo différent ?</a>
