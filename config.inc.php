8783cedf4475d250b2e45f91206552b34368f685
Gaël Foppolo <me@gaelfoppolo.com>
2014-12-22 10:22:06 +0100

    ajout constante du mail du site + double check fonction connecté

--- config.inc.php
+++ config.inc.php
@@ -6,10 +6,11 @@ define('VIEW_PATH', ROOT.DS.'vue'.DS);
 define('CTR_PATH', ROOT.DS.'controleur'.DS);
 define('MODEL_PATH', ROOT.DS.'modele'.DS);
 define('VIEW_PATH_BASE', BASE.'vue/');
+define('SITEEMAIL','no_reply@pfcls.me');
 
 session_start();
 
 // vérifier si l'utilisateur est connecté
 function estConnecte() {
-    return(isset($_SESSION['idJoueur']));
+    return(isset($_SESSION['idJoueur']) && !empty($_SESSION['idJoueur']));
 }
