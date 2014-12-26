8e9ae04655eae9c22a028d54dc1f889bc66f6e24
Gaël Foppolo <me@gaelfoppolo.com>
2014-12-26 18:57:30 +0100

    ajout description projet

--- vue/index/vueAproposIndex.php
+++ vue/index/vueAproposIndex.php
@@ -1,3 +1,34 @@
+<h2 id="mainhead"><span class="fa fa-bar-chart"></span> Le projet</h2>
+<hr>
+<div class="col-md-12">
+  <p>Le jeu <em>Pierre Feuille Ciseaux Lézard Spock</em> est un jeu non-coopératif fondamental, opposant deux joueurs,
+    où il faut affronter son adversaire à l'aide de cinq « figures » que sont la pierre, la feuille,
+    les ciseaux, le lézard et Spock et en suivant <a href="index.php?action=regles">quelques règles simples</a>.
+  </p>
+  <p>
+    Chaque personne a une façon particulière de jouer, adoptant un comportement et des stratégies
+    qui lui sont propre. Partant de ce constat, les joueurs essaient de comprendre les séquences de
+    coups de leur adversaire pour s'y opposer et trouver un modèle émergent. Notre projet s'inscrit
+    dans cet optique, mais poussé à l'extrême en établissant des statistiques sur l'utilisation
+    des différentes figures. Grâce à ces statistiques, notre IA élabore la meilleure stratégie de jeu
+    pour chaque joueur afin de le battre de façon optimale à chaque coup.
+  </p>
+  <p>
+    Mais d'un point de vue plus général, nous nous intéressons à la découverte de motifs contextuels
+    dans les séquences de coups de façon à mettre en lumière des corrélations cachées dans les données
+    ou des tendances de jeu générales en fonctions du contexte. Nous avons définies deux paramètres contextuels : l'<strong>âge</strong> et le <strong>sexe</strong>.
+    En partant de l'idée générale qu'un motif fréquent représente un comportement attendu, nous nous
+    sommes posés les questions suivantes :
+  </p>
+  <p><em>Peut-on prévoir, à partir d'un motif fréquent et de données
+    contextuelles, le prochain coup qui sera joué ?</em><br/><em>Peut-on déduire, à partir d'un motif
+    fréquent et de données contextuelles, une tendance de jeu ?</em>
+  </p>
+  <p>
+    Les résultats sont disponibles sur la page <a href="index.php?action=statistiques"><span class="fa fa-pie-chart"></span> Statistiques</a>
+  </p>
+</div>
+
 <h2 id="mainhead"><span class="fa fa-users"></span> L'équipe</h2>
 <hr>
 <div class="row">
@@ -73,9 +104,3 @@
             </div>
           </div>
       </div>
-<h2 id="mainhead"><span class="fa fa-bar-chart"></span> Le projet</h2>
-<hr>
-  <div class="col-md-12">
-    <p> description du projet à réaliser en séparant le partie jeu et la partie IA
-    </p>
-  </div>
