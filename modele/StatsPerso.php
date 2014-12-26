b113c2958f781ed8952a253c94bef2bfd3eb8f87
dumartinetj <julien.dumartinet@dumartinetj.fr>
2014-12-26 17:10:11 +0100

    IA 1er coup Working (CF description)
    si le bonhomme a déjà joué alors on choppe la figure qu'il a le plus
    joué, et on joue un random des 2 figures qui battent sa figure la plus
    joué. Si il a jamais joué, le premier coup est un random

---
+++ modele/StatsPerso.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Classe StatsPerso
+ */
+
+require_once 'Modele.php';
+
+class StatsPerso extends Modele {
+
+  protected static $table = "pfcls_StatistiquesPersonnelles";
+  protected static $primary_index = "idStatsPerso";
+
+}
+
+?>
