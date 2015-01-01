127fd96c8704a8c6c470fbebb91d8bc89015a28c
Gaël Foppolo <me@gaelfoppolo.com>
2015-01-01 21:41:09 +0100

    fix erreurs d'affichage des boutons sur petit écran

--- vue/joueur/vueDeleteJoueur.php
+++ vue/joueur/vueDeleteJoueur.php
@@ -5,9 +5,9 @@
 <p><small>Êtes-vous sûr de vouloir supprimer votre compte définitivement ?<br/>
 Toutes vos données seront effacées définitivement et il ne sera plus possible de les récupérer...</small></p>
 <span class="fa-frown-o fa-5x"></span>
-<p><a href="joueur.php?action=profil" class="btn btn-lg btn-info"><span class="fa fa-heart"></span> Revenir sur votre profil</a></p>
+<p><a href="joueur.php?action=profil" class="btn btn-info"><span class="fa fa-heart"></span> Revenir sur votre profil</a></p>
 <form method="post" action="joueur.php?action=deleted">
-  <input class="btn btn-xs btn-danger" type="submit" name="submit" value=" &#xf1f8; Confirmer la suppression définitive"/>
+  <input class="btn btn-xs btn-danger" type="submit" name="submit" value=" &#xf1f8; Confirmer la suppression"/>
 </form>
 </div>
 </div>
