d9d89422224c518948dde37cdb0ad4599a356dbd
Gaël Foppolo <me@gaelfoppolo.com>
2015-01-11 21:11:09 +0100

    migration de la fonction premierCoup() dans le modele + controleur ready pour appel fonction IA

--- modele/JeuIA.php
+++ modele/JeuIA.php
@@ -2,49 +2,85 @@
 
 require_once 'Modele.php';
 require_once 'Partie.php';
-require_once MODEL_PATH.'Joueur.php'; 
+require_once MODEL_PATH.'Joueur.php';
 
 class JeuIA extends Modele{
-	
-	protected static $table = "pfcls_Parties_en_attente";
-	protected static $primary_index = "idPartie_en_attente";
-	
-	
-	/*public static function recupSequence(){//permet de récup la séquence de coup en cour du joueur 
+
+	protected static $table = "pfcls_StatistiquesPersonnelles";
+	protected static $primary_index = "idStatsPerso";
+
+	public static function premierCoup($idJoueur){
+		$dataDejaJoue = array('idJoueur'=>$idJoueur);
+		$dejaJoue=StatsPerso::selectWhere($dataDejaJoue);
+		if($dejaJoue!=NULL){
+			$listeCoupsJoueur="";
+			foreach ($dejaJoue as $key => $value) {
+				$listeCoupsJoueur .= str_replace(',', '', $value->listeCoups);
+			}
+			$figureCount = array(
+				'1'=>substr_count($listeCoupsJoueur,'1',0,strlen($listeCoupsJoueur)),
+				'2'=>substr_count($listeCoupsJoueur,'2',0,strlen($listeCoupsJoueur)),
+				'3'=>substr_count($listeCoupsJoueur,'3',0,strlen($listeCoupsJoueur)),
+				'4'=>substr_count($listeCoupsJoueur,'4',0,strlen($listeCoupsJoueur)),
+				'5'=>substr_count($listeCoupsJoueur,'5',0,strlen($listeCoupsJoueur))
+			);
+			$nbOccumax=$figuremax=0;
+			foreach($figureCount as $figure => $nbOccu){
+				if($nbOccu>$nbOccumax){
+					$nbOccumax=$nbOccu;
+					$figuremax=$figure;
+				}
+			}
+			$dataFaiblesses = array('idFigure'=>$figuremax);
+			$faiblesses=Figure::select($dataFaiblesses)->faiblesses;
+			$valeurs = explode(",",$faiblesses);
+			$faiblesserandom = array_rand($valeurs);
+			$choixFigure = $valeurs[$faiblesserandom];
+		}
+		else{
+			$choixFigure = mt_rand(1,5);
+		}
+		return $choixFigure;
+	}
+
+
+
+
+	/*public static function recupSequence(){//permet de récup la séquence de coup en cour du joueur
 		//if (estConnecte()){
 		    $data = array("listeCoups" => "%".$sequence."%");
 			try{
-			
+
 			 $req = self::$pdo->prepare('SELECT listeCoups FROM pfcls_statistiquespersonnelles WHERE listeCoups LIKE :listeCoups');
 			 $req -> execute($data);
 			} catch (PDOException $e) {
                 echo $e->getMessage();
                 $messageErreur="Erreur lors de la récupération de la séquence";
             }
-        
+
 		//}
-            
+
             $resultat = $pdo->query($requete);
             $check=$resultat->fetch(PDO::FETCH_NUM);
-             
-            
-            
+
+
+
     }
 	*/
-	
+
 
     public static function reducSeq($sequence){
-		
+
         return $sequenceRed = substr($sequence,2);
-        
+
     }
-	
-    
-	
-	
+
+
+
+
     public static function occurence($arrayValeur){
         $a = $b = $c = $d = $e = 0;
-		
+
         for($i=0;$i<count($arrayValeur);$i++){
             if ((strcasecmp($arrayValeur[$i],"1"))==0){
                 $a=$a+1;
@@ -57,30 +93,30 @@ class JeuIA extends Modele{
             } elseif ((strcasecmp($arrayValeur[$i],"5"))==0) {
                 $e=$e+1;
             }
-			
-			
+
+
         }
-					
+
 		$arrayValeur=array(1 => $a,$b,$c,$d,$e);
-						
+
 		return array_search(max($arrayValeur),$arrayValeur);
-		
-        
+
+
     }
-	
+
 	public static function coupSuiv(array $array, $sequence){
          // on va y mettre les séquences qui ont une valeur après la séquence que l'ont recherche
-		
+
         for($i=0;$i<count($array);$i++){
 			if(strstr($array[$i], $sequence ,  $before_sequence = false)!=false){
-            $arrayValeurs[] = strstr($array[$i], $sequence ,  $before_sequence = false); //ajoute les séquence qui contiennent la séquence recherché 
-            //avec la séquence recherchée en début de chaine de caractère et la suite de la chaine. 
+            $arrayValeurs[] = strstr($array[$i], $sequence ,  $before_sequence = false); //ajoute les séquence qui contiennent la séquence recherché
+            //avec la séquence recherchée en début de chaine de caractère et la suite de la chaine.
             //exemple: on cherche la séquence 123 dans la chaine 241312345 on obtiendra la chaine 12345 dans le tableau.
 			}
 			else{
-				
+
 			}
-			
+
         }
         for($j=0;$j<count($arrayValeurs);$j++){
 			if(strlen(substr($arrayValeurs[$j],strlen($sequence)+1))>1){
@@ -94,9 +130,9 @@ class JeuIA extends Modele{
 				$arrayValeur[] = substr($arrayValeurs[$j],strlen($sequence)+1);
 			}
         }
-		
+
         return JeuIA::occurence($arrayValeur); // renvoie le nombre d'occurence du caractère q
-    } 
+    }
 
 }
 
