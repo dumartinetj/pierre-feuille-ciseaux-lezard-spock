06f2691b45348dbc0febd2939badbe6fbbe0064a
Gaël Foppolo <me@gaelfoppolo.com>
2015-01-12 20:50:05 +0100

    new icons plus cool

--- vue/index/vueStatistiquesIndex.php
+++ vue/index/vueStatistiquesIndex.php
@@ -35,8 +35,8 @@
             </label>
           </div><br/>
           <div class="input-group"><span class="input-group-addon"><i class="fa fa-calculator"></i></span><input type="number" class="form-control" placeholder="Âge" name="age" id="id_age" min="1" max="100" required/></div><br/>
-          <div class="input-group"><span class="input-group-addon"><i class="fa fa-calculator"></i></span><input type="number" class="form-control" placeholder="Marge" name="marge" id="id_marge" min="0" max="10" required/></div><br/>
-          <input type="submit" class="btn btn-default btn-lg" value="&#xf00c; Générer les statistiques !" />
+          <div class="input-group"><span class="input-group-addon"><i class="fa fa-arrows-h"></i></span><input type="number" class="form-control" placeholder="Marge" name="marge" id="id_marge" min="0" max="10" required/></div><br/>
+          <input type="submit" class="btn btn-default btn-lg" value="&#xf021; Générer les statistiques" />
         </fieldset>
       </form>
     </div>
