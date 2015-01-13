c03a728ab2116aed35ea35f51e53a9bd71845a28
Gaël Foppolo <me@gaelfoppolo.com>
2015-01-13 12:04:52 +0100

    fix non responsive items

--- vue/index/vueStatistiquesIndex.php
+++ vue/index/vueStatistiquesIndex.php
@@ -19,7 +19,6 @@
       <div class="row">
       <div class="col-md-offset-3 col-md-6">
       <form method="post" action="index.php?action=stats">
-        <fieldset>
           <div>
             <input type="radio" name="sexe" id="homme" value="H" required/>
             <label for="homme">
@@ -40,8 +39,7 @@
           </div><br/>
           <div class="input-group"><span class="input-group-addon"><i class="fa fa-calculator"></i></span><input type="number" class="form-control" placeholder="Âge" name="age" id="id_age" min="1" max="100" required/></div><br/>
           <div class="input-group"><span class="input-group-addon"><i class="fa fa-arrows-h"></i></span><input type="number" class="form-control" placeholder="Marge" name="marge" id="id_marge" min="0" max="10" required/></div><br/>
-          <input type="submit" class="btn btn-default btn-lg" value="&#xf021; Générer les statistiques" />
-        </fieldset>
+          <input type="submit" class="btn btn-default" value="&#xf021; Générer les statistiques" />
       </form>
     </div>
   </div>
--- vue/joueur/vueRecoveryJoueur.php
+++ vue/joueur/vueRecoveryJoueur.php
@@ -3,10 +3,8 @@
 <div class="row">
 <div class="col-md-offset-3 col-md-6">
 <form method="post" action="joueur.php?action=recoveredpwd">
-    <fieldset>
             <div class="input-group"><span class="input-group-addon"><i class="fa fa-envelope"></i></span><input type="email" class="form-control" placeholder="E-mail" name="email" id="id_email" required/></div><br/>
             <input type="submit" class="btn btn-default" value="&#xf00c; Remettre à zéro" />
-    </fieldset>
 </form>
 </div>
 </div>
